33503b5a8c09f7d03f8b3b2336e4004226dbe9f4
lcorderof <luiscorderof.developer@gmail.com>
2018-01-07 12:49:46 -0500

    tercer envio

--- 2/app.js
+++ 2/app.js
@@ -3,6 +3,19 @@ var bodyparser = require('body-parser');
 var app = express();
 var puerto = 3000;
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/paises");
+var userSchemaJSON ={
+	nombre : String,
+	apellido : String,
+	email : String,
+	edad : Number
+};
+var user_schema = new Schema(userSchemaJSON);
+var User = mongoose.model('User', user_schema);
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -27,9 +40,18 @@ app.get('/registro', (req, res) => {
 
 app.post('/registrar', (req, res) => {
 	// res.render('./registro');
-	console.log(req.body);
+	// console.log(req.body);
+	var user = new User ({
+		nombre : req.body.nombre,
+		apellido : req.body.apellido,
+		email : req.body.email,
+		edad : req.body.edad
+	});
+	user.save(function(){
+		res.send('Usuario registrado');
+	});
 });
 
 app.listen(puerto, () => {
-	console.log("V5 - Ejecutandose en el ṕuerto ", puerto);
+	console.log("V6 - Ejecutandose en el ṕuerto ", puerto);
 });
